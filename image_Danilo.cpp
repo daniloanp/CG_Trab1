ac60df540bee341ad3276ec3c17a408f24e74290
Danilo <danilo_anp@archlinux.com.br>
2015-09-28 17:42:29 -0300

    Fixing things

--- image_Danilo.cpp
+++ image_Danilo.cpp
@@ -1158,9 +1158,6 @@ in middle position, but other elements are NOT sorted.
         Pixel p;
 
 
-        Image* imgFinal = this->Binary();
-
-
         //First Step
         for (y = 0; y < height; y++) {
             for (x = 0; x < width; x++) {
